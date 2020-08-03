fa7cbe707cd6ec1bd5ee9bb8d5838a5ec3da1b05
alberto56 <albert@mediatribe.net>
2020-08-03 13:44:36 -0400

    remove body toc

--- docs/my-assets/js/toc.js
+++ docs/my-assets/js/toc.js
@@ -7,15 +7,10 @@ function simulate_hamburger_click() {
   $('.slicknav_btn').click();
 }
 
-function append_toc(anchor, identifier, css_class, text) {
-  $(identifier).append('<li class="' + css_class + '"><a href="#' + anchor + '">' + text + '</a></li>');
-}
-
 $(function() {
   $('.single-post h2').each(function() {
     var anchor = text_to_anchor($(this).text());
     $(this).before('<div class="anchor-wrapper"><a id="' + anchor + '"></a></div>');
-    append_toc(anchor, '#toc', '', $(this).text());
     $('.mobile_menu .mobile-toc-template-li').first().removeClass('template').removeClass('mobile-toc-template-li').html('<a onclick="simulate_hamburger_click()" href="#' + anchor + '">' + $(this).text() + '</a>');
   });
 });
