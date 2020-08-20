1df68dcd879f6139a4695aa82c25c82d4c5c3a86
alberto56 <albert@mediatribe.net>
2020-08-20 12:59:32 -0400

    better footer and contact us link

--- docs/my-assets/js/toc.js
+++ docs/my-assets/js/toc.js
@@ -7,10 +7,17 @@ function simulate_hamburger_click() {
   $('.slicknav_btn').click();
 }
 
+function add_to_toc(selector) {
+  var anchor = text_to_anchor(selector.text());
+  selector.before('<div class="anchor-wrapper"><a id="' + anchor + '"></a></div>');
+  $('.mobile_menu .mobile-toc-template-li').first().removeClass('template').removeClass('mobile-toc-template-li').html('<a onclick="simulate_hamburger_click()" href="#' + anchor + '">' + selector.text() + '</a>');
+}
+
 $(function() {
   $('.single-post h2').each(function() {
-    var anchor = text_to_anchor($(this).text());
-    $(this).before('<div class="anchor-wrapper"><a id="' + anchor + '"></a></div>');
-    $('.mobile_menu .mobile-toc-template-li').first().removeClass('template').removeClass('mobile-toc-template-li').html('<a onclick="simulate_hamburger_click()" href="#' + anchor + '">' + $(this).text() + '</a>');
+    add_to_toc($(this));
+  });
+  $('.footer-toc-item').each(function() {
+    add_to_toc($(this));
   });
 });
