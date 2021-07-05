e621e55c6e3829d104a66b7eb32100d6ced95cd7
Albert Albala <albert@mediatribe.net>
2021-07-05 16:12:56 -0400

    2021 design

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -3,10 +3,14 @@ set -e
 
 ./scripts/destroy.sh
 ./scripts/build-static-site.sh
-docker run --rm -d --name lehub -p 8081:80 -v "$PWD/docs/_site":/usr/local/apache2/htdocs/ httpd:2.4
 
-echo ""
-echo "Visitez http://0.0.0.0:8081 pour voir le site localement."
-echo ""
-echo "Utilisez ./scripts/destroy.sh pour arrêter l'environnement local."
-echo ""
+source ./config/versioned
+
+docker network ls | grep "$DOCKERNETWORK" || docker network create "$DOCKERNETWORK"
+
+docker run --rm -d \
+  --name "$DOCKERNAME" \
+  --network "$DOCKERNETWORK" \
+  -p "$DOCKERPORT":80 -v "$PWD/docs/_site":/usr/local/apache2/htdocs/ httpd:2.4
+
+./scripts/uli.sh
